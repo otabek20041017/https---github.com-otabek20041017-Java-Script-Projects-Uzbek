57b46582cc906f280e790b883f7c5d6638793950
Otabek Ozeg <otabek.yusupov20041017@gmail.com>
2024-04-05 11:54:30 -0700

    Currency Converter

---
+++ currency converter/index.js
@@ -0,0 +1,30 @@
+const currencyFirstEl = document.getElementById("currency-first");
+
+const worthFirstEl = document.getElementById("worth-first");
+
+const currencySecondEl = document.getElementById("currency-second");
+
+const worthSecondEl = document.getElementById("worth-second");
+
+const exchangeRateEl = document.getElementById("exchange-rate");
+
+updateRate()
+
+function updateRate(){
+    fetch(`https://v6.exchangerate-api.com/v6/a7e9217356360cb587120f4a/latest/${currencyFirstEl.value}`)
+    .then((res) => res.json())
+    .then((data) => {
+        const rate = data.conversion_rates[currencySecondEl.value];
+        console.log(rate);
+        exchangeRateEl.innerText = `1 ${currencyFirstEl.value} = ${rate + " " + currencySecondEl.value}`;
+
+        worthSecondEl.value = (worthFirstEl.value * rate).toFixed(2)
+    });
+}
+
+currencyFirstEl.addEventListener("change", updateRate)
+
+currencySecondEl.addEventListener("change", updateRate
+)
+
+worthFirstEl.addEventListener("input", updateRate)
