c55afd9596c3404d7dd2ae8c3ab106c3a6f67e31
Otabek Ozeg <otabek.yusupov20041017@gmail.com>
2024-05-16 22:58:19 +0500

    emoji rating

---
+++ emoji rating/index.js
@@ -0,0 +1,25 @@
+const starsEl = document.querySelectorAll(".fa-star");
+const emojisEl = document.querySelectorAll(".fa-regular");
+const colorsArray = ["red","orange", "lightblue", "lightgreen", "green"]
+
+starsEl.forEach((starsEl, index) => {
+    starsEl.addEventListener("click", ()=>{
+        updateRating(index)
+    })
+})
+
+
+function updateRating(index){
+    starsEl.forEach((starsEl, idx)=>{
+        if (idx < index + 1) {
+            starsEl.classList.add("active");
+        } else{
+            starsEl.classList.remove("active");
+        }
+    });
+
+    emojisEl.forEach((emojisEl)=>{
+        emojisEl.style.transform = `translateX(-${index * 50}px)`;
+        emojisEl.style.color = colorsArray[index];
+    });
+}
