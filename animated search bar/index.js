5845517997c355e60dd7e0146a1b83e185f1d604
Otabek Ozeg <otabek.yusupov20041017@gmail.com>
2024-05-21 13:30:39 +0500

    animated search bar

---
+++ animated search bar/index.js
@@ -0,0 +1,10 @@
+const searchBarContanierEl = document.querySelector(".search-bar-container");
+const magnifierEl = document.querySelector(".magnifier");
+
+magnifierEl.addEventListener("click", ()=>{
+    searchBarContanierEl.classList.toggle("active");
+})
+
+
+
+
